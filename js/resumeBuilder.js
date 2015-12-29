406112fbac61e03dfa7a829cd36a81a84f13d012
David B. Wetzel <david@familycatproductions.com>
2015-12-29 09:03:28 -0600

    add formatted name and role to header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,26 @@
-$("#main").append("David B. Wetzel, DMA");
+// $("#main").append("David B. Wetzel, DMA <br>");
+
+// var awesomeThoughts = "I am Possum, and I am AWESOME!"
+
+// console.log(awesomeThoughts);
+
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+// console.log(funThoughts);
+
+// $("#main").append(funThoughts);
+
+var formattedName = "David B. Wetzel, DMA";
+
+var formattedRole = "Musician, Educator, Web & Interactive Software Developer";
+
+HTMLheaderName = HTMLheaderName.replace("%data%", formattedName);
+HTMLheaderRole = HTMLheaderRole.replace("%data%", formattedRole);
+
+$("#header").append(HTMLheaderName);
+
+$("#header").append(HTMLheaderRole);
+
+
+
+
