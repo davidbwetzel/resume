2e12d255ebc306a7bd8a347dba340f7a30d0243f
David B. Wetzel <david@familycatproductions.com>
2016-01-05 15:17:39 -0600

    Clean up code and create object literals

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,25 +1,171 @@
-// $("#main").append("David B. Wetzel, DMA <br>");
+var bio = {
+			"name" : "David B. Wetzel, DMA", 
+			"role": "Musician, Educator, Web & Interactive Software Developer",
+			"contact" : {
+						"email" : "david@familycatproductions.com",
+						"mobile" : "773-484-9357",
+						"github" : "dbwetzel",
+						"web" : "davidbrookewetzel.net",
+						"location" : "Chicago"
+					},
+			"bioPic" : "http://davidbrookewetzel.net/wp-content/uploads/2014/09/dbwetzel-1.jpg",
+			"welcome" : "Howdy",
+			"skills" : [
+						"performance", 
+						"teaching", 
+						"programming", 
+						"web development", 
+						"audio production"
+						]
+		};
 
-// var awesomeThoughts = "I am Possum, and I am AWESOME!"
+var projects = 
+{
+	"personal" : [
+		{
+			"name" : "Interactive Event Manager",
+			"platform" : "Max/MSP",
+			"start" : 2007,
+			"current" : true
+		},
+		{
+			"name" : "Resonance",
+			"platform" : "Web Browser",
+			"start" : 2014,
+			"current" : true
+		}
+	],
+	"Udacity" : [
+		{
+			"name" : "Web Portfolio"
+		},
+		{
+			"name" : "Interactive Resume"
+		}
+	]
+}
 
-// console.log(awesomeThoughts);
+var work = 
+{
+	"positions" : [
+		{
+			"employer" : "Educational Testing Service (ETS)",
+			"position" : "test rater",
+			"areas" : ["CAASSP", "TOEFL Speaking", "TOEIC Speaking"],
+			"current" : true,
+			"start" : "March 2015",
+			"end" : "present",
+			"years" : 0.5
+		},
+		{
+			"employer" : "Mansfield University of Pennsylvania",
+			"parent" : "Pennsylvania State System of Higher Education (PASSHE)",
+			"position" : "associate professor of music",
+			"previous" : "assistant professor of music",
+			"department" : "music",
+			"distinctions" : [
+				"tenure (2009)",
+				"promotion in rank (2010)",
+				"outstanding senior mentor (2014)"
+				],
+			"teaching" : ["clarinet", "music technology", "general music"],
+			"current" : false,
+			"start" : "August 2004",
+			"end" : "August 2014",
+			"years" : 10
+		}
 
-// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+	]
+};
 
-// console.log(funThoughts);
+var education = 
+{
+	"schools" : 
+	{
+		"HS" : 
+		{
+			"name" : "Lane TechnicalHigh School",
+			"institution" : "Chicago Public Schools",
+			"degree" : "Diploma",
+			"year" : "1987",
+			"attended" : "1983 - 1987",
+			"location" : "Chicago, IL",
+			"major" : "Math & Science"
+		},
 
-// $("#main").append(funThoughts);
+		"UG" : 
+		{
+			"name" : "Lawrence Conservatory of Music",
+			"institution" : "Lawrence University",
+			"city" : "Appleton, WI",
+			"degree" : "Bachelor of Music",
+			"dgr" : "B. Mus.",
+			"year" : "1992",
+			"attended" : "1987 - 1992",
+			"major" : "clarinet performance",
+			"minor" : "BA studies in history (degree incomplete)",
+			"advisors" : 
+			[
+				"Dan C. Sparks", 
+				"William F. Chaney"
+			]
+		},
 
-var formattedName = "David B. Wetzel, DMA";
-
-var formattedRole = "Musician, Educator, Web & Interactive Software Developer";
-
-formattedName = HTMLheaderName.replace("%data%", formattedName);
-formattedRole = HTMLheaderRole.replace("%data%", formattedRole);
-
-$("#header").prepend(formattedRole);
-
-$("#header").prepend(formattedName);
+		"grad" : 
+		[
+			{
+				"name" : "Peabody Conservatory of Music",
+				"institution" : "The Johns Hopkins University",
+				"city" : "Baltimore, MD",
+				"degree" : "Master of Music",
+				"dgr" : "MM",
+				"year" : "1999",
+				"attended" : "1994 - 1999",
+				"major" : "computer music performance and concert production",
+				"advisors" : 
+				[
+					"McGregor Boyle",
+					"Ichiro Fujinaga", 
+					"Geoffrey Wright"
+				]
+			}, 
+			{
+				"name" : "School of Music and Dance",
+				"institution" : "the University of Arizona",
+				"city" : "Tucson, AZ",
+				"degree" : "Doctor of Musical Arts",
+				"dgr" : "DMA",
+				"year" : "2004",
+				"attended" : "2001 - 2004",
+				"major" : "clarinet performance",
+				"minor" : "music in the general curriculum",
+				"advisors" : 
+				[
+					"Jerry E. Kirkbride",
+					"Janet Sturman"
+				]
+			}
+		]
+	},
+	"CEU" : 
+	[
+	{
+		"name" : "Udacity",
+		"program" : "Nanodegree",
+		"subject" : "Front End Web Developer",
+		"year" : "2016",
+		"attended" : "2015 - 2016",
+		"online courses" : 
+		[
+			"Intro to HTML and CSS", 
+			"Responsive Web Design", 
+			"Responsive Images",
+			"How to Use GIT and GitHub",
+			"JavaScript Basics"
+		]
+	}
+	]	
+};
 
 
 
